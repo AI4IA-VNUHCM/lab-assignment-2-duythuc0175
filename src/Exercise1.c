5f289d9c7b1b67dde7469f2ab1ab6c2c28e56a5e
Nguyễn Trần Duy Thức ITITIU20318 <thuccau1072002@gmail.com>
2021-10-11 14:42:16 +0700

    update bai1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -19,6 +19,21 @@ int main(int argc, char *argv[]) {
 	float distance = atof(argv[1]);
 	int result;
 	//Your codes here
+	if(distance<=2){
+    result=15000;
+}
+ else if(distance>2&&distance<=30)
+{
+  distance=distance-2;
+  result=15000+((distance*2000)/0.25);
+
+
+}
+else if(distance>30)
+{
+    result=15000 + (((30-2)*2000)/0.25) +  ((distance-30)*5000);
+
+}
 	
 	printf("%d", result);
 	return 0;
